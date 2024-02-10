21414f08c857ba6a365cacf5f80eb508702e7844
Arjun Kumawat <98701361+Adkumawat@users.noreply.github.com>
2024-02-10 22:08:10 +0530

    Update index.blade.php

--- resources/views/products/index.blade.php
+++ resources/views/products/index.blade.php
@@ -13,13 +13,9 @@
                             </div>
                         @endif
 
-                        <div>
+                        <div class="row">
                             <div class="col-2 left">
                                 <a href="{{ route('products.create') }}" class="btn btn-primary">Add New Product</a>
-                            </div>
-
-                            <div class="col-2 right">
-                                <input type="text" name="search" class="form-control" placeholder="Search" id="search" value="{{request()->get('search') }}" style="width:unset;">
                             </div>
                         </div>
                         <table class="table">
